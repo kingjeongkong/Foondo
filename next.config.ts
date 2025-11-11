8ed1db52124c7b4bbf05471646d2dc1234e13f2d
kingjeongkong <wjdqls9223@gmail.com>
2025-11-11 22:13:00 +0900

    wip

--- next.config.ts
+++ next.config.ts
@@ -1,32 +1,6 @@
 import type { NextConfig } from 'next';
-// @ts-ignore - 플러그인이 타입 선언을 제공하지 않음
-const { PrismaPlugin } = require('@prisma/nextjs-monorepo-workaround-plugin');
 
 const nextConfig: NextConfig = {
-  webpack: (config, { isServer }) => {
-    if (isServer) {
-      config.plugins = [...(config.plugins || []), new PrismaPlugin()];
-    }
-    return config;
-  },
-  outputFileTracingIncludes: {
-    // 모든 경로에 Prisma 파일 포함 (API 라우트뿐만 아니라)
-    '/': [
-      // Prisma Query Engine 바이너리 파일들을 명시적으로 포함
-      'src/generated/prisma/libquery_engine-rhel-openssl-3.0.x.so.node',
-      'src/generated/prisma/libquery_engine-rhel-openssl-1.1.x.so.node',
-      'src/generated/prisma/libquery_engine-rhel-openssl-1.0.x.so.node',
-      // 모든 Prisma 생성 파일 포함
-      'src/generated/prisma/**',
-    ],
-    // API 라우트에도 명시적으로 포함
-    '/api/**': [
-      'src/generated/prisma/libquery_engine-rhel-openssl-3.0.x.so.node',
-      'src/generated/prisma/libquery_engine-rhel-openssl-1.1.x.so.node',
-      'src/generated/prisma/libquery_engine-rhel-openssl-1.0.x.so.node',
-      'src/generated/prisma/**',
-    ],
-  },
   images: {
     remotePatterns: [
       {
