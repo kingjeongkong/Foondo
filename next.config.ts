5cf7e3f35a06939e8c032b1a7a29e7bb4e85a9df
kingjeongkong <wjdqls9223@gmail.com>
2025-11-11 21:16:14 +0900

    wip

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,14 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
+  outputFileTracingIncludes: {
+    // 모든 API 라우트에 대해
+    '/api/**': [
+      // schema.prisma에 설정된 'output' 경로 내의
+      // 모든 쿼리 엔진 파일을 강제로 포함시킵니다.
+      './src/generated/prisma/**',
+    ],
+  },
   images: {
     remotePatterns: [
       {
